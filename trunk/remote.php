e8d81781ed11e8e1d8ee821eb32f3cdf3ec33878
keithamus@gmail.com <keithamus@gmail.com@3051db82-1d7c-11df-8d00-ad93a377a9c4>
2010-03-09 14:51:06 +0000

    This revision was mostly improvements to the sort menu. Now all torrents use data-* attributes, making the sort much more robust and faster (before it set a "sort" attribute for every time a sort was needed).
    remote.php and rtorrent.php were cleaned up to make way for the new setsettings.php, which gives serverside rewrite functionality to prefs.json. This change has enabled Avalanche to retain settings such as if the details pane is open, and the method used to sort the torrents.
    
    Also, this change brings about a license change to GPL v3, from the old MIT. Although more restrictive this should encourage developers to build upon our codebase and is the right decision in moving forward to a robust Avalanche. As part of this change the LICENSE.txt has been added.
    
    This release also marks beta 4 or Avalanche - so all users should update.

--- trunk/remote.php
+++ trunk/remote.php
@@ -4,13 +4,29 @@
 	 *
 	 * Avalanche 0.9 Beta
 	 *
-	 *	Copyright 2009 © Keithamus
-	 *	This code is licensed under the MIT license.
-	 *	For more details, see http://www.opensource.org/licenses/mit-license.php
+	 *	This code is licensed under the GPL3, or "GNU GENERAL PUBLIC LICENSE Version 3"
+	 *	For more details, see http://opensource.org/licenses/gpl-3.0.html
 		*
 		* For more information, see http://code.google.com/p/avalanche-rt
 		*
-		* Date: Fri, 19th Feb 2010.
+		* Date: Tue, 09th Mar 2010.
+		*
+		* @author Keith Cirkel ('keithamus') <avalanche@keithcirkel.co.uk>
+		* @license http://opensource.org/licenses/gpl-3.0.html
+		* @copyright Copyright © 2010, Keith Cirkel
+		*
+		* This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		*
 	 */
 
@@ -79,8 +95,6 @@ switch($_GET['action'])
 	case 'setlabel':
 		$contents=$server->SetLabel($_GET['id'],$_GET['value']);
 		break;
-	case 'getsettings':
-		break;
 	case 'getpeers':
 		$contents=$server->GetPeers($_GET['id']);
 		break;
@@ -115,22 +129,6 @@ switch($_GET['action'])
 		$contents=$server->Retrieve();
 		break;
 
-}
-
-function write_settings_file($file, $settings)
-{
-	$handler = fopen($file, 'w');
-	if($handler)
-	{
-		$settings = json_encode($settings);
-		fwrite($handler, $settings);
-		fclose($handler);
-		die($settings);
-	}
-	else
-	{
-		die(json_encode(array('error'=>'Couldn\'t open file for writing') ) );
-	}
 }
 
 //Use die to ensure script executes and no extra whitespace is sent (just incase)
--- trunk/scripts/init.js
+++ trunk/scripts/init.js
@@ -13,15 +13,15 @@
 		*
 	 */
 
-//Initialise our "torrent" object.
-var torrent;
-
 //When DOM is ready...
 $(document).ready( function() {
 
 	//Create our settings object, we should set some defaults to stop potential bugs
 	window.settings = {
 		refresh_delay: 10000,
+		default_sort: 'new_name',
+		default_sort_by: 'asc',
+		detail_pane_open: true,
 		web_searches: [{
 			name: 'Google',
 			url: 'http://www.google.com/search?hl=en&q=%s+filetype:torrent&btnG=Search'
@@ -31,7 +31,11 @@ $(document).ready( function() {
 	//Load our settings into our global settings object:
 	//SIDE: We'd love to use getJSON, but getJSON silently fails - which is crap
 	$.ajax({url:'prefs.json', dataType: 'json', success: function(data) {
-		window.settings = data;
+		window.settings = $.extend(window.settings, data);
+		if(data.detail_pane_open==false || data.detail_pane_open=='false')
+		{
+			torrent.toggleDetailPane();
+		}
 		//Populate the search menu
 		$.each(data.web_searches, function(i, search)
 		{
@@ -64,116 +68,3 @@ $(document).ready( function() {
 	});
 
 });
-
-
-//Some common functions we'll use alot:
-
-/*
- * Math.formatbytes
- *
- * Change bytes into an appropriate human SI figure (like GB)
- *
- * @argument bytes {integer} The bytes to retun as SI figure.
- *
- * @returns {string} bytes as an SI figure (e.g "3.00 GB")
- */
-Math.formatBytes = function(bytes)
-{
-	if(bytes==0) return '0 B';
-	var size;
-	var unit;
-
-	// Size is 1 or more Terabytes (TB).
-	if ( bytes >= 1099511627776 )
-	{
-
-		size = bytes / 1099511627776;
-		unit = ' TB';
-
-	// Size is 1 or more Gigabytes (GB).
- }
-	else if ( bytes >= 1073741824 )
-	{
-
-		size = bytes / 1073741824;
-		unit = ' GB';
-
-	// Size is 1 or more Megabytes (MB).
-	}
-	else if ( bytes >= 1048576 )
-	{
-
-		size = bytes / 1048576;
-		unit = ' MB';
-
-	// Size is 1 or more Kilobytes (KB).
- }
-	else if ( bytes >= 1024 )
-	{
-
-		size = bytes / 1024;
-		unit = ' KB';
-
-	// The size is less than one KB
-	}
-	else
-	{
-
-		size = bytes;
-		unit = ' B';
-
-	}
-
-	size = size>0?size.toFixed(2):0;
-
-	//Make it say 0 instead of 0.00
-	if (size == 0.00) {
-		size = 0;
-	}
-
-	return size + unit;
-}
-
-/*
- *   Converts seconds to more readable units (hours, minutes etc).
- *
- *   @param seconds {integer} The number of seconds to format
- *   @returns {string} Seconds into redable Days, Minutes, Hours.
- */
-Math.formatSeconds = function(seconds)
-{
-	//Start with an empty result var:
-	var result = '';
-
-	//See if the result is months long
-	var num = Math.floor(seconds / 2629743);
-	if (num > 0) result += num+ ' month(s), ';
-
-	//See if the result is weeks long:
-	num = Math.floor((seconds % 2629743) / 604800);
-	if (num > 0) result += num+ ' week(s), ';
-
-	//See if the result is days long:
-	num = Math.floor((seconds % 604800) / 86400);
-	if (num > 0) result += num+ ' day(s), ';
-
-	//See if the result is hours long (minus the days)
-	num = Math.floor((seconds % 86400) / 3600);
-	if (num > 0) result += num+ ' hour(s), ';
-
-	//See if the result is minutes long (minus the hours)
-	num = Math.floor((seconds % 3600) / 60);
-	if (num > 0) result += num+ ' min(s), ';
-
- return result+ Math.floor((seconds % 3600) % 60)+ ' seconds';
-}
-
-/*
- * :icontains selector
- *
- * Just a quick new selector, same as ":contains" except it is case insensitive!
- *
- */
-$.expr[':'].icontains = function(obj, index, meta, stack){
-return (obj.textContent || obj.innerText || jQuery(obj).text() || '').toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
-};
